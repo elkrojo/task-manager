8b8cbc73785748d7ba49fd7b3d34841f91af909e
Ronan C <rojocas@gmail.com>
2020-02-14 09:38:33 +0000

    Added category editing functionality

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ def update_task(task_id):
                     'category_name': request.form.get('category_name'),
                     'task_description': request.form.get('task_description'),
                     'due_date': request.form.get('due_date'),
-                    'is_urgent': request.form.get('is_urgent')
-                })
+                    'is_urgent': request.form.get('is_urgent')})
+
     return redirect(url_for('get_tasks'))
 
 
@@ -58,6 +58,18 @@ def update_task(task_id):
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
+
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
 
 
 if __name__ == '__main__':
